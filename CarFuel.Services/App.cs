7a4650afd1dfbcf389a72c05dfc77b2ba232ebaa
Pramot Samranwong <endwindows07@gmail.com>
2019-07-24 16:03:32 +0700

    add Lazy

---
+++ CarFuel.Services/App.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using CarFuel.Services.Data;
+
+namespace CarFuel.Services
+{
+    public class App
+    {
+        private Lazy<MemberService> memberService;
+
+        public App(AppDB db)
+        {
+            memberService = new Lazy<MemberService>(() => new MemberService(db));
+            Cars = new CarService(db);
+            Db = db;
+        }
+
+        public MemberService Members => memberService.Value;
+
+        public CarService Cars { get; set; }
+        public AppDB Db { get; }
+
+        public int SaveChanged() => Db.SaveChanges();
+        public Task<int> SaveChangedAsyns() => Db.SaveChangesAsync();
+    }
+}
